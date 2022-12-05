dee7af3d21b3aaf88c4c21581fa9df1966a48308
Alon Mlievski <alonmlievski@gmail.com>
2022-12-05 15:50:34 +0200

    modal-rendering completed

--- js/proj-controller.js
+++ js/proj-controller.js
@@ -7,12 +7,12 @@ function onInit() {
 function renderProjects() {
   var projects = getProjects();
   var elPortfolioCaption = document.querySelector(".row.portfolio-cards");
-var elModalBody = document.querySelector(".modal-body");
+  var elModalBody = document.querySelector(".modal-section");
 
   var captionHTML = projects
     .map(
       (project) => `<div class="col-md-4 col-sm-6 portfolio-item">
-      <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+      <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${project.id}" onclick="openModal('${project.id}')">
         <div class="portfolio-hover">
           <div class="portfolio-hover-content">
             <i class="fa fa-plus fa-3x"></i>
@@ -28,51 +28,84 @@ var elModalBody = document.querySelector(".modal-body");
     )
     .join("");
 
-// var modalHTML = projects
-// .map(
-//   (project) => `<h2>${project.title}</h2>
-//   <p class="item-intro text-muted">
-//     Lorem ipsum dolor sit amet consectetur.
-//   </p>
-//   <img
-//     class="img-fluid d-block mx-auto"
-//     src="img/portfolio/01-full.jpg"
-//     alt=""
-//   />
-//   <p>
-//   ${project.desc}
-//   </p>
-//   <ul class="list-inline">
-//     <li>${project.publishedAt}</li>
-//     <li>Client: Threads</li>
-//     <li>Category: Illustration</li>
-//   </ul>`
-// )
-// .join("");
-
+  console.log(projects.id);
   elPortfolioCaption.innerHTML = captionHTML;
   // elModalBody.innerHTML = modalHTML
 }
 
+// function getId(ev) {
+//   console.log(ev);
+// }
 
-{/* <h2>Project Name</h2>
-<p class="item-intro text-muted">
-  Lorem ipsum dolor sit amet consectetur.
-</p>
-<img
-  class="img-fluid d-block mx-auto"
-  src="img/portfolio/01-full.jpg"
-  alt=""
-/>
-<p>
-  Use this area to describe your project. Lorem ipsum dolor
-  sit amet, consectetur adipisicing elit. Est blanditiis
-  dolorem culpa incidunt minus dignissimos deserunt repellat
-  aperiam quasi sunt officia expedita beatae cupiditate,
-  maiores repudiandae, nostrum, reiciendis facere nemo!
-</p>
-<ul class="list-inline">
-  <li>Date: January 2017</li>
-  <li>Client: Threads</li>
-  <li>Category: Illustration</li>
-</ul> */}
+function openModal(id) {
+  var projects = getProjects();
+  console.log(projects);
+  var filtered = projects.filter((project) => id === project.id);
+  console.log(filtered[0].id);
+
+  renderModal(filtered)
+}
+
+function renderModal(obj){
+  console.log(obj);
+  var elModalSection = document.querySelector('.modal-section')
+  var modalHTML = `<div
+  class="portfolio-modal modal fade"
+  id="portfolioModal${obj[0].id}"
+  tabindex="-1"
+  role="dialog"
+  aria-hidden="true"
+>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <!-- Project Details Go Here -->
+              <h2>${obj[0].title}</h2>
+              <p class="item-intro text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+              <img
+                class="img-fluid d-block mx-auto"
+                src="img/portfolio/01-full.jpg"
+                alt=""
+              />
+              <p>
+                Use this area to describe your project. Lorem ipsum dolor
+                sit amet, consectetur adipisicing elit. Est blanditiis
+                dolorem culpa incidunt minus dignissimos deserunt repellat
+                aperiam quasi sunt officia expedita beatae cupiditate,
+                maiores repudiandae, nostrum, reiciendis facere nemo!
+              </p>
+              <ul class="list-inline">
+                <li>Date: January 2017</li>
+                <li>Client: Threads</li>
+                <li>Category: Illustration</li>
+              </ul>
+              <button
+                class="btn btn-primary"
+                data-dismiss="modal"
+                type="button"
+              >
+                <i class="fa fa-times"></i>
+                Close Project
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+elModalSection.innerHTML = modalHTML
+
+}
+
+
